fdf3e5b26c23fc42566142c755cb0a7bcbd33f5e
Andrzej Stolarek <stolar.code@gmail.com>
2020-11-26 18:42:54 +0100

    first functions added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log("Hello world!")
+{
+    const tasks = [
+        {
+            content: "something1",
+            done: true,
+        },
+        {
+            content: "something2",
+            done: false,
+        }
+    ];
+
+    const render = () => {
+
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="list__item">
+            <button class="list__button list__button--done">Done</button>${task.content}
+            <button class="list__button list__button--delete">Delete</button>
+            </li>
+            `
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
